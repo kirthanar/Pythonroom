a98c84685e7c1910d38b03bd64f644742cf0a818
kirthanar <kirthana.r2@gmail.com>
2014-04-26 13:59:57 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -3,13 +3,13 @@
 import turtle
 
 length = 100
-angle = 90
-#numbers= range(0,6)
-colors = ["red", "blue", "green", "yellow"]
+angle = 170
+numbers= range(0,100)
+#colors = ["red", "blue", "green", "yellow"]
 
 kirthana = turtle.Turtle()
 
-for color in colors:
-	kirthana.color(color)
+for number in numbers:
+	
 	kirthana.forward(length)
 	kirthana.left(angle)
