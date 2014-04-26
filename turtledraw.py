f03aacb2a91a6e4506db656fc118ca171ebc55af
kirthanar <kirthana.r2@gmail.com>
2014-04-26 13:18:50 -0700

    pythonroom update

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,19 @@
 # author: kirthanar
+
+import turtle
+
+length = 100
+angle = 72
+
+kirthana = turtle.Turtle()
+
+kirthana.forward(length)
+kirthana.left(angle)
+kirthana.forward(length)
+kirthana.left(angle)
+kirthana.forward(length)
+kirthana.left(angle)
+kirthana.forward(length)
+kirthana.left(angle)
+kirthana.forward(length)
+kirthana.left(angle)
